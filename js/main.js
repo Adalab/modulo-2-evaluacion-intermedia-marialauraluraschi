fdc60537b2e0c89e182c14bbf34d6f3128b2284d
Maria Laura Luraschi <loud.luraschi@gmail.com>
2023-09-19 09:14:01 +0200

    constantes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const select= document.querySelector('.js-select');
+const btn= document.querySelector('.js-btn');
+const score= document.querySelector('.js-score');
+const player= document.querySelector('.js-player');
+const pc= document.querySelector('.js-pc');
+
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
    }
