625f1d58fd04a06766a2cd36e643f6aa492dceed
Lau Luraschi <loud.luraschi@gmail.com>
2023-09-19 11:20:17 +0200

    "time"

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ const btn= document.querySelector('.js-btn');
 const score= document.querySelector('.js-score');
 const player= document.querySelector('.js-player');
 const pc= document.querySelector('.js-pc');
-const rstBtn=document.querySelector('.rst-btn');
 
 let playerScore= 0;
 let pcScore= 0;
