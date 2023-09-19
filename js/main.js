bddf8a34d371b77e4cde68576285947a4740f292
Maria Laura Luraschi <loud.luraschi@gmail.com>
2023-09-19 09:52:29 +0200

    function for the computer to play

--- js/main.js
+++ js/main.js
@@ -6,7 +6,31 @@ const score= document.querySelector('.js-score');
 const player= document.querySelector('.js-player');
 const pc= document.querySelector('.js-pc');
 
+let playerScore= 0;
+let pcScore=0;
+
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
    }
-   
+
+function pcSelection() {
+    const randNum = getRandomNumber(9);
+    if (randNum <= 3) {
+        return 'piedra';
+    } else if (randNum >= 7) {
+        return 'papel';
+    } else {
+        return 'tijera';
+    }
+}
+
+function play(){
+    const playerSelection=select.value;
+}
+
+function handleClick(event){
+    event.preventDefault();
+    play();
+}
+
+btn.addEventListener('click', handleClick);
